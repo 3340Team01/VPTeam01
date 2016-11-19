bc419b5c8250b2898585be3e0b1a3f25517843cc
MichelleMGarcia <michelle.m.garcia01@utrgv.edu>
2016-11-18 19:31:25 -0600

    Updated CREATE TABLE Reminders

--- Demo1/src/Database/DBOperations.java
+++ Demo1/src/Database/DBOperations.java
@@ -147,7 +147,7 @@ public class DBOperations {
            String sql1="CREATE TABLE Courses";
            stmt.executeQuery(sql1);
            
-           String sql2="CREATE TABLE Reminders";
+           String sql2="CREATE TABLE Reminders (reminderName varchar(20), message varchar(256), currentTime time, primary key (messageName))";
            stmt.executeQuery(sql2);
            
           
